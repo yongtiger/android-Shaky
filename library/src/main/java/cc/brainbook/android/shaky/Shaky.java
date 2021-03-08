56ac2b5b8e3544097eade3d150ad482715f03e60
Robert Han <brainbook.cc@outlook.com>
2021-03-08 02:37:24 -0800

    v0.0.2#import Falcon, ShakeDetector

--- library/src/main/java/cc/brainbook/android/shaky/Shaky.java
+++ library/src/main/java/cc/brainbook/android/shaky/Shaky.java
@@ -1,18 +1,3 @@
-/**
- * Copyright (C) 2016 LinkedIn Corp.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package cc.brainbook.android.shaky;
 
 import android.app.Activity;
@@ -33,11 +18,11 @@ import androidx.annotation.UiThread;
 import androidx.annotation.VisibleForTesting;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
-import com.jraska.falcon.Falcon;
-import com.squareup.seismic.ShakeDetector;
-
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
+
+import cc.brainbook.android.shaky.util.Falcon;
+import cc.brainbook.android.shaky.util.ShakeDetector;
 
 /**
  * Listens for a shake and then starts the feedback submission flow.
